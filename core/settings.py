84e86d23fe04f72ecd055bbc76b504e5c2be9ab9
jml-moe <2318069@student.itn.ac.id>
2025-12-14 09:23:53 +0700

    Add explicit WhiteNoise configuration

--- core/settings.py
+++ core/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"  # Folder untuk collected static files
 
 # WhiteNoise configuration untuk production
